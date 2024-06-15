d9545c86cc49514f9d05d4146df7fb10f6efc34d
unknown <samirphanuel@gmail.com>
2024-06-15 18:35:31 +0100

    v2

--- src/context/category pages/SingleArticlePath.jsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-sportPath = createContext('/sport/:slug')
-motivationPath = createContext('/motivation/:slug')
-relationManWomanPath = createContext('/relation-homme-femme/:slug')
