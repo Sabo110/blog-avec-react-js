660b3e441d825c90892e25107ea712721572dad1
unknown <samirphanuel@gmail.com>
2024-06-16 14:57:07 +0100

    ajustement des hauteur des sections des single article

--- src/components/single article/FirstSection.jsx
+++ src/components/single article/FirstSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function FirstSection(props) {
   const {article} = props
   return (
-    <section className="bg-white h-[400px] flex justify-center items-center">
+    <section className="bg-white md:h-[400px] h-[300px] flex justify-center items-center">
       <div className="content lg:w-1/2 md:w-3/4 w-11/12">
         <h1 className="lg:text-5xl md:text-2xl text-2xl text-center mb-4 text-transparent bg-clip-text bg-gradient-to-r from-[#5ce1e6] to-[#00bf63] font-bold"> {article.title} </h1>
         <p className="md:text-lg text-center"> <span className="font-light">{article.createDate}</span> . <span className="capitalize font-bold">{article.category}</span> </p>
--- src/components/single article/SecondSection.jsx
+++ src/components/single article/SecondSection.jsx
@@ -5,13 +5,13 @@ function SecondSection(props) {
     return (
         <section className="bg-white">
             <div className="lg:w-3/4 md:w-11/12 md:mx-auto pb-10">
-                <div className="bg-gradient-to-r from-indigo-300 via-purple-500 to-pink-100 h-[500px]"><img src={article.img1} alt="premiere image" className="w-full h-full"/></div>
+                <div className=" md:h-[500px] h-[300px]"><img src={article.img1} alt="premiere image" className="w-full h-full"/></div>
                 <div className="md:w-3/4 mx-auto w-11/12 flex flex-col gap-4 my-8">
                     <p> {article.p1} </p>
                     <p> {article.p2} </p>
                     <p> {article.p3} </p>
                 </div>
-                <div className="md:w-4/5 mx-auto w-11/12 bg-gradient-to-r from-pink-100 to-blue-300 h-[390px]"><img src={article.img2} alt="deuxieme image" className="w-full h-full" /></div>
+                <div className="md:w-4/5 mx-auto w-11/12  md:h-[390px] h-[250px]"><img src={article.img2} alt="deuxieme image" className="w-full h-full" /></div>
                 <div className="md:w-3/4 mx-auto w-11/12 flex flex-col gap-4 my-8">
                     <p> {article.p4} </p>
                     <p> {article.p5} </p>
